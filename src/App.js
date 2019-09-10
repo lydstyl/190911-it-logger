37e4829907f545803b0569433e2bff80dd548c46
Gabriel Brun <lydstyl@gmail.com>
2019-09-10 22:44:25 +0200

    materialize and clean up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
 
 const App = () => {
+  useEffect(() => {
+    // Init Materialize JS
+    M.AutoInit();
+  }); // Why not [] as second arg ? Let's try it later to see if it works
+
   return <div className='App'>My app</div>;
 };
 
