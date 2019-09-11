0ab8871e926e16de7169e8d1a916d2c8b440a17a
Gabriel Brun <lydstyl@gmail.com>
2019-09-11 19:11:29 +0200

    DELETE_LOG

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,26 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Moment from 'react-moment';
 import PropTypes from 'prop-types';
+import { deleteLog } from '../../actions/logAction';
 
-const LogItem = ({ log }) => {
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+const LogItem = ({ log, deleteLog }) => {
+  const { id, attention, message, tech, date } = log;
+
+  const onDelete = () => {
+    deleteLog(id);
+    M.toast({ html: 'Log deleted' });
+  };
+
   return (
     <li className='collection-item'>
       <div>
         <a
           href='#edit-log-modal'
-          className={`modal-trigger ${
-            log.attention ? 'red-text' : 'blue-text'
-          }`}
+          className={`modal-trigger ${attention ? 'red-text' : 'blue-text'}`}
         >
-          {log.message}
+          {message}
         </a>
         <br />
         <span className='grey-text'>
-          <span className='black-text'>ID #{log.id}</span> last updated by{' '}
-          <span className='black-text'>{log.tech}</span> on{' '}
-          <Moment format='MMMM Do YYYY, h:mm:ss a'>{log.date}</Moment>
+          <span className='black-text'>ID #{id}</span> last updated by{' '}
+          <span className='black-text'>{tech}</span> on{' '}
+          <Moment format='MMMM Do YYYY, h:mm:ss a'>{date}</Moment>
         </span>
-        <a href='#!' className='secondary-content'>
+        <a href='#!' className='secondary-content' onClick={onDelete}>
           <i className='material-icons grey-text'>delete</i>
         </a>
       </div>
@@ -29,7 +38,11 @@ const LogItem = ({ log }) => {
 };
 
 LogItem.propTypes = {
-  log: PropTypes.object.isRequired
+  log: PropTypes.object.isRequired,
+  deleteLog: PropTypes.func.isRequired
 };
 
-export default LogItem;
+export default connect(
+  null,
+  { deleteLog } // 5 add the action if you have one as a second parameter
+)(LogItem);
