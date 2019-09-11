727bd2ada315fbf0de410f2401a61a6d9deac6d1
Gabriel Brun <lydstyl@gmail.com>
2019-09-11 17:22:44 +0200

    types, logActions & logReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,6 @@
 import { combineReducers } from 'redux';
+import logReducer from './logReducer';
 
-export default combineReducers({});
+export default combineReducers({
+  log: logReducer // log is what we call our state just for the logReducer
+});
