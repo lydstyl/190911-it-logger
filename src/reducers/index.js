9a90d6b8298fb252b20e770d0e70037bd9798d66
Gabriel Brun <lydstyl@gmail.com>
2019-09-12 14:04:57 +0200

    Techs action, reducer & component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import logReducer from './logReducer';
+import techReducer from './techReducer';
 
 export default combineReducers({
-  log: logReducer // log is what we call our state just for the logReducer
+  log: logReducer, // log is what we call our state just for the logReducer
+  tech: techReducer
 });
